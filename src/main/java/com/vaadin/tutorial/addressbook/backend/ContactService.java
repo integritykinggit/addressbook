ebcef58a1eb95b06ca1ccb975455e10a18345f1a
integritykinggit <111797191+integritykinggit@users.noreply.github.com>
2022-09-15 20:13:15 +0100

    Update ContactService.java

--- src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/ContactService.java
@@ -19,11 +19,11 @@ public class ContactService {
     static String[] fnames = { "Freeman", "Victor", "Treasure", "Peter", "Alice", "John", "Mike", "Olivia",
             "Nina", "Alex", "Rita", "Dan", "Umberto", "Henrik", "Rene", "Lisa",
             "Linda", "Timothy", "Daniel", "Brian", "George", "Scott",
-            "Jennifer" };
+            "Jennifer", "Precious" };
     static String[] lnames = { "Smith", "Johnson", "Williams", "Jones",
             "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor",
             "Anderson", "Thomas", "Jackson", "White", "Harris", "Martin",
-            "Thompson", "Young", "King", "Robinson" };
+            "Thompson", "Young", "King", "Robinson", "Eze" };
 
     private static ContactService instance;
 
